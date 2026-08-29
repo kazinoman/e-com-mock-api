66ef85fce01cb9045ed208f60c70437844407dcc
Abdullah al Noman <kazinoman9999@gmail.com>
2026-08-29 19:03:08 +0600

    chore: update default server port from 3000 to 3001

--- server.js
+++ server.js
@@ -240,7 +240,7 @@ server.use((req, res, next) => {
 // Use default router
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`JSON Server is running at http://localhost:${PORT}`);
 });
